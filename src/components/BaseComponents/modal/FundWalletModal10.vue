c7ba1c8fc536f1d4465ce332addf0c1585f8f2d4
Dagbue <dagbuel@gmail.com>
2025-09-07 13:42:06 +0100

    first push

--- src/components/BaseComponents/modal/FundWalletModal10.vue
+++ src/components/BaseComponents/modal/FundWalletModal10.vue
@@ -190,7 +190,7 @@ export default {
       inputValue1: '',
       inputValue2: '',
       inputValue3: 119000,
-      inputValue4: 1593,
+      inputValue4: 1070,
       bitcoinRate: null,
       ethereumRate: null,
       completed: false,
